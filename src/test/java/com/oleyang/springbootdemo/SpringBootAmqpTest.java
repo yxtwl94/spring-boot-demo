5d3df9092bfe95d8a7f186b245e4a21bb1f49d30
oleyang <oleyang@tencent.com>
2022-03-22 16:25:46 +0800

    取消测试

--- src/test/java/com/oleyang/springbootdemo/SpringBootAmqpTest.java
+++ src/test/java/com/oleyang/springbootdemo/SpringBootAmqpTest.java
@@ -7,27 +7,27 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class SpringBootAmqpTest {
-    @Autowired
-    RabbitTemplate rabbitTemplate;
-
-    @Test
-    public void testSendDirectMsg(){
-        String queueName = "simple.queue";
-        String msg = "hello";
-        // 注入消息，阅后即焚类型direct
-        rabbitTemplate.convertAndSend(queueName, msg);
-    }
-
-    // work queue模式，一个队列对应多个消费者处理
-    @Test
-    public void testSendWorkQueueMsg() throws InterruptedException {
-        String queueName = "simple.queue";
-
-        // work queue模拟，产生50个数据
-        for (int i = 0; i < 50; i++) {
-            String msg = "hello_" + i;
-            rabbitTemplate.convertAndSend(queueName, msg);
-        }
-    }
+//    @Autowired
+//    RabbitTemplate rabbitTemplate;
+//
+//    @Test
+//    public void testSendDirectMsg(){
+//        String queueName = "simple.queue";
+//        String msg = "hello";
+//        // 注入消息，阅后即焚类型direct
+//        rabbitTemplate.convertAndSend(queueName, msg);
+//    }
+//
+//    // work queue模式，一个队列对应多个消费者处理
+//    @Test
+//    public void testSendWorkQueueMsg() throws InterruptedException {
+//        String queueName = "simple.queue";
+//
+//        // work queue模拟，产生50个数据
+//        for (int i = 0; i < 50; i++) {
+//            String msg = "hello_" + i;
+//            rabbitTemplate.convertAndSend(queueName, msg);
+//        }
+//    }
 
 }
